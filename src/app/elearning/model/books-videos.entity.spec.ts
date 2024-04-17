61618addbf5d1823a83cb362ff2da08837245710
Eric <u202213358@upc.edu.pe>
2024-04-16 19:37:56 -0500

    feat(Api Services): added Api Services, book-videos entity

---
+++ src/app/elearning/model/books-videos.entity.spec.ts
@@ -0,0 +1,7 @@
+import { BooksVideosEntity } from './books-videos.entity';
+
+describe('BooksVideosEntity', () => {
+  it('should create an instance', () => {
+    expect(new BooksVideosEntity()).toBeTruthy();
+  });
+});
